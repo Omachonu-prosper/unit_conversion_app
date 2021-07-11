0904b9dfa5a9fdc4ac0652dabc9c7aac949c60c6
Omachonu-prosper <omachonucodes@gmail.com>
2021-07-11 20:57:05 +0100

    Set up the container/main section

--- js/modules/page_layout.js
+++ js/modules/page_layout.js
@@ -1,21 +1,34 @@
 class Elements {
-    constructor () {
+    constructor (unitName, originalUnit, convertingUnit) {
         this.nav = `
             <div class="nav">
                 <h1 class="brand">Unit Converter</h1>
             </div>
         `; 
+        this.container = `
+            <section>
+                <div class="container">
+                    <h2 class="unit-title">
+                        ${unitName}
+                        <span>
+                            (${originalUnit} to ${convertingUnit})
+                        </span>
+                    </h2>
+                </div>
+            </section>
+        `;
     }
 }
 
-// this function gathers all page elements and arranges them how they ought to be 
-function arrangeElements() {
-    const elements = new Elements()
+// this function gathers all page elements and arranges them how they ought to be on default page load
+function pageLoadElements() {
+    const elements = new Elements('Width', 'meter', 'centimeter')
    
     return `
         ${elements.nav}
+        ${elements.container}
     `
 }
 
 // export the arranged elements 
-export const arrange = arrangeElements();
+export const arrange = pageLoadElements();
