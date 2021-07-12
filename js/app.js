eb6fa13d8a9f1bd65a6751dfa11065779168e2ea
Omachonu-prosper <omachonucodes@gmail.com>
2021-07-12 23:25:58 +0100

    Create an event controller IIFE

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 // imports 
 import arrange from './modules/page_layout.js';
+import interactivity from './modules/interactive.js';
 
 // App variables
 const  root = document.querySelector('#root');
 
 // set the innerHTML of the root element to the value returned from arrange 
 root.innerHTML = arrange;
+
+// event listeners 
+const EventCtrl = ( () => {
+    document.querySelector('#pDDButton').addEventListener( 'click', interactivity.togglePrimaryDropDown );
+})()
