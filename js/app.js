ee12c2927be5c6dd7b08f8a56837bcc0ec1c274d
Omachonu-prosper <omachonucodes@gmail.com>
2021-07-22 14:28:05 +0100

    Undo chnages of last commit

--- js/app.js
+++ js/app.js
@@ -7,14 +7,7 @@ import arrange from './modules/page_layout.js';
 import Interactivity from './modules/interactive.js';
 
 // App variables
-const root = document.querySelector('#root'),
-    input = document.querySelector('#unitValue'),
-    title = document.querySelector('#unit-title'),
-    selector1 = document.querySelector('#unit-title'),
-    selector2 = document.querySelector('#unit-title'),
-    outputElement = document.querySelector('.output'),
-    primaryDropdownButton = document.querySelector('#pDDButton'),
-    primaryDropdownBox = document.querySelector('#pDDBox');
+const  root = document.querySelector('#root');
 
 // set the innerHTML of the root element to the value returned from arrange 
 root.innerHTML = arrange;
@@ -42,12 +35,13 @@ const AppFunctions = ( () => {
     return{
         convert: () => {
             // the Number form ot the value inputed by the user
-            const value = Number(input.value);
-            const category = title.dataset.name;
+            const value = Number(document.querySelector('#unitValue').value);
+            const category = document.querySelector('#unit-title').dataset.name;
             // the select1's selected options value
-            const from = selector1.selectedOptions[0].value;
+            const from = document.querySelector('#s1').selectedOptions[0].value;
             // the select2's selected options value
-            const to = selector2.selectedOptions[0].value;
+            const to = document.querySelector('#s2').selectedOptions[0].value;
+            const outputElement = document.querySelector('.output');
             
             // Send the answer to the user
             value === 0 ? outputElement.innerHTML = '0.00' : outputElement.innerHTML =  decideConversion(category, value, from, to);
@@ -57,21 +51,19 @@ const AppFunctions = ( () => {
 
 // event listeners 
 const EventCtrl = ( () => {
-
     // on page load fill in the values for unit-title and secondary drop down content 
     document.addEventListener( 'DOMContentLoaded', Interactivity.pageLoadContent );
 
     // click on the units to change from one unit to another 
-    primaryDropdownBox.addEventListener( 'click', Interactivity.changeUnit )
+    document.querySelector('#pDDBox').addEventListener( 'click', Interactivity.changeUnit )
 
     // listen for a click to toggle the primary dropdown 
-    primaryDropdownButton.addEventListener( 'click', Interactivity.togglePrimaryDropDown );
+    document.querySelector('#pDDButton').addEventListener( 'click', Interactivity.togglePrimaryDropDown );
 
     // listen for when a user types in the input(#unitValue);
-    input.addEventListener('keyup', AppFunctions.convert);
+    document.querySelector('#unitValue').addEventListener('keyup', AppFunctions.convert);
 
     // once a selector(and its outputs) is/are clicked it is assumed that the user might have changed the converting to or from unit so we would convert and output to correspond to the supposed change 
-    selector1.addEventListener('click', AppFunctions.convert);
-    selector2.addEventListener('click', AppFunctions.convert);
-
+    document.querySelector('#s1').addEventListener('click', AppFunctions.convert);
+    document.querySelector('#s2').addEventListener('click', AppFunctions.convert);
 })();
