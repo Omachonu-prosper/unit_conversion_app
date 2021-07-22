73d42d3435fb6a60c212e7f244901bcdc04167b3
Omachonu-prosper <omachonucodes@gmail.com>
2021-07-22 14:20:34 +0100

    Create variables to enforce DRY codes

--- js/app.js
+++ js/app.js
@@ -7,7 +7,14 @@ import arrange from './modules/page_layout.js';
 import Interactivity from './modules/interactive.js';
 
 // App variables
-const  root = document.querySelector('#root');
+const root = document.querySelector('#root'),
+    input = document.querySelector('#unitValue'),
+    title = document.querySelector('#unit-title'),
+    selector1 = document.querySelector('#unit-title'),
+    selector2 = document.querySelector('#unit-title'),
+    outputElement = document.querySelector('.output'),
+    primaryDropdownButton = document.querySelector('#pDDButton'),
+    primaryDropdownBox = document.querySelector('#pDDBox');
 
 // set the innerHTML of the root element to the value returned from arrange 
 root.innerHTML = arrange;
@@ -35,13 +42,12 @@ const AppFunctions = ( () => {
     return{
         convert: () => {
             // the Number form ot the value inputed by the user
-            const value = Number(document.querySelector('#unitValue').value);
-            const category = document.querySelector('#unit-title').dataset.name;
+            const value = Number(input.value);
+            const category = title.dataset.name;
             // the select1's selected options value
-            const from = document.querySelector('#s1').selectedOptions[0].value;
+            const from = selector1.selectedOptions[0].value;
             // the select2's selected options value
-            const to = document.querySelector('#s2').selectedOptions[0].value;
-            const outputElement = document.querySelector('.output');
+            const to = selector2.selectedOptions[0].value;
             
             // Send the answer to the user
             value === 0 ? outputElement.innerHTML = '0.00' : outputElement.innerHTML =  decideConversion(category, value, from, to);
@@ -51,19 +57,21 @@ const AppFunctions = ( () => {
 
 // event listeners 
 const EventCtrl = ( () => {
+
     // on page load fill in the values for unit-title and secondary drop down content 
     document.addEventListener( 'DOMContentLoaded', Interactivity.pageLoadContent );
 
     // click on the units to change from one unit to another 
-    document.querySelector('#pDDBox').addEventListener( 'click', Interactivity.changeUnit )
+    primaryDropdownBox.addEventListener( 'click', Interactivity.changeUnit )
 
     // listen for a click to toggle the primary dropdown 
-    document.querySelector('#pDDButton').addEventListener( 'click', Interactivity.togglePrimaryDropDown );
+    primaryDropdownButton.addEventListener( 'click', Interactivity.togglePrimaryDropDown );
 
     // listen for when a user types in the input(#unitValue);
-    document.querySelector('#unitValue').addEventListener('keyup', AppFunctions.convert);
+    input.addEventListener('keyup', AppFunctions.convert);
 
     // once a selector(and its outputs) is/are clicked it is assumed that the user might have changed the converting to or from unit so we would convert and output to correspond to the supposed change 
-    document.querySelector('#s1').addEventListener('click', AppFunctions.convert);
-    document.querySelector('#s2').addEventListener('click', AppFunctions.convert);
+    selector1.addEventListener('click', AppFunctions.convert);
+    selector2.addEventListener('click', AppFunctions.convert);
+
 })();
